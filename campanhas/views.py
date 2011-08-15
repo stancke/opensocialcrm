89e501bf43ab1a8471770fd8bf41ca9333d8a405
Wilian Stancke <w.stancke@gmail.com>
2011-08-14 23:36:27 -0300

    Consulta e lançamentos de campanhas implementados

--- campanhas/views.py
+++ campanhas/views.py
@@ -1,10 +1,44 @@
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from campanhas.models import Campanhas
 
 def index(request):
 
-    return render_to_response('campanhas/index.html')
+    if request.user.is_authenticated():
+        campanhas = Campanhas.objects.all()
+        total = Campanhas.objects.count()
+    
+        return render_to_response('campanhas/index.html', {"campanhas" :campanhas, "total":total})
+    else:
+        return render_to_response('erros/autenticacao.html')
 
 def nova(request):
+    
+    if request.user.is_authenticated():
 
-    return render_to_response('campanhas/nova.html')
+        return render_to_response('campanhas/nova.html')
+    else:
+        return render_to_response('erros/autenticacao.html')
+
+def cadastrarNova(request):
+    
+    if request.user.is_authenticated():
+    
+        if request.method == 'POST':
+                string = request.POST
+                
+                tit = string.get("titulo")
+                desc = string.get("descricao")
+                redes = string.getlist("redes_sociais")
+                arrayRedes = []
+                for rede in redes:
+                    arrayRedes.append(rede)
+
+                p = Campanhas(redes_sociais= arrayRedes, titulo=tit, descricao=desc)
+                p.save()
+        
+        return HttpResponseRedirect("/campanhas/")
+    
+    else:
+        
+        return render_to_response('erros/autenticacao.html')
