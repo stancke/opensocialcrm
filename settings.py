2cc62fee2918ea798c55c0846dae845b406bb1c6
Wilian Stancke <w.stancke@gmail.com>
2011-08-20 00:26:04 -0300

    alteracoe em settings

--- settings.py
+++ settings.py
@@ -26,7 +26,10 @@ DATABASES = {
 
 TIME_ZONE = 'America/Sao_Paulo'
 
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'pt-br'
+
 
 USE_I18N = True
 USE_L10N = True
