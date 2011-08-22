ab5d3652634213bf535d65f21a99dae51e109aba
Wilian Stancke <w.stancke@gmail.com>
2011-08-22 19:36:57 -0300

    commit de atualizações

--- urls.py
+++ urls.py
@@ -10,7 +10,6 @@ urlpatterns = patterns('',
 
      (r'^resultados/(?P<url>\w+)/$', 'opensocialcrm.resultados.views.index'),
      url(r'^$', 'opensocialcrm.index.views.index'),
-     url(r'^erro_autenticacao/$', 'opensocialcrm.index.views.erroAutenticacao'),
      (r'^grappelli/', include('grappelli.urls')),
      (r'^media/(.*)$', 'django.views.static.serve', {'document_root': os.path.join(settings.PROJECT_PATH, 'media')}),
      (r'^static/(.*)$', 'django.views.static.serve', {'document_root': os.path.join(settings.PROJECT_PATH, 'static')}),
